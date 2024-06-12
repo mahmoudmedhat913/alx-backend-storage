badac56ec84f469f37928422c9eacd68835a07b5
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-06-12 11:13:16 -0700

    101-average_weighted_score.sql

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,17 @@
+-- Write sql script
+
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser()
+BEGIN
+	UPDATE users AS users,
+	       (SELECT users.id,SUM(score * weight) / SUM(weight) AS weight_avg
+	       FROM users AS users JOIN corrections AS correct ON users.id = correct.user_id
+	       JOIN projects AS proj ON correct.project_id = proj_id
+	       GROUP BY users.id)
+
+	AS weight
+	SET users.average_score = weight.weight_avg
+	WHERE users.id = weight.id;
+END $$
+DELIMITER ;
