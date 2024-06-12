c13e3c739980ac70137fef798d5a47284e97590f
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-06-12 11:17:13 -0700

    101-average_weighted_score.sql

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -7,7 +7,7 @@ BEGIN
 	UPDATE users AS users,
 	       (SELECT users.id, SUM(score * weight) / SUM(weight) AS weight_avg
 	       FROM users AS users JOIN corrections AS correct ON users.id = correct.user_id
-	       JOIN projects AS proj ON correct.project_id = proj_id
+	       JOIN projects AS proj ON correct.project_id = proj.id
 	       GROUP BY users.id)
 
 	AS weight
