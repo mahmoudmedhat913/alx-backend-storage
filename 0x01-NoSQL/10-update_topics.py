5e3ac5f0892e370088781cac8e02f1656adf3238
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-06-15 05:50:46 -0700

    10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,4 +6,4 @@ import pymongo
 def update_topics(mongo_collection, name, topics):
     """change all topics"""
     return mongo_collection.update_many({"name": name},
-                                        {"$set": {"topics": topic}})
+                                        {"$set": {"topics": topics}})
